b9ba89c3487621cde2311bdd85bbcb19aa963e77
ebeni <abenikeb79@gmail.com>
2021-09-04 21:20:11 +0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Movies from './components/movies'
+import NavBar from './components/NavBar'
+import Customers from './components/customers'
+import Rental from './components/rental'
+import NotFound from './components/notFound'
+import MovieForm from './components/movieForm'
+import LoginForm from './components/loginForm'
+import Register from './components/register'
+import Logout from './components/logout'
+import auth from './services/authService'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+import ProtectedRoute from './components/common/protectedRoute'
+
+class App extends Component {
+  state = {}
+  componentDidMount() {
+    const user = auth.getUserData()
+    this.setState({ user })
+  }
+  render() {
+    const { user } = this.state
+    return (
+      <div>
+        <ToastContainer />
+        <NavBar user={user} />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rental" component={Rental} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
